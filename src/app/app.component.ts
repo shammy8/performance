a5caaa58cac6dca54f7b54607d1b45245cc2cb41
allansham <allan.sham@trapezegroup.com>
2021-08-29 01:30:03 +0100

    Add toolbar to the top of page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` <button mat-flat-button color="primary">Test</button> `,
+  template: `
+    <mat-toolbar>
+      <button mat-icon-button [matMenuTriggerFor]="menu">
+        <mat-icon>menu</mat-icon>
+      </button>
+      <h1>Angular Performance Optimisation</h1>
+      <!-- <nav><a>Employee</a></nav> -->
+    </mat-toolbar>
+    <mat-menu #menu="matMenu">
+      <a mat-menu-item>Employee Lists</a>
+      <a mat-menu-item>Other</a>
+    </mat-menu>
+  `,
   styles: [],
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
